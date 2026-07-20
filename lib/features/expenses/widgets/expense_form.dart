15bafd86c9c3c0000f8d136221a81d6c91a85010
SreekarMovva <sreekarmovva@gmail.com>
2026-07-20 13:19:28 +0530

    Added form validation and improved expense form UX.

--- lib/features/expenses/widgets/expense_form.dart
+++ lib/features/expenses/widgets/expense_form.dart
@@ -22,6 +22,8 @@ class _ExpenseFormState extends ConsumerState<ExpenseForm> {
 
   final TextEditingController dateController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   String selectedCategory = 'Food';
   DateTime selectedDate = DateTime.now();
   @override
@@ -41,116 +43,147 @@ class _ExpenseFormState extends ConsumerState<ExpenseForm> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        TextField(
-          controller: nameController,
-          decoration: const InputDecoration(
-            labelText: 'Expense Name',
-          ),
-        ),
-        SizedBox(height: 12),
-        TextField(
-          controller: amountController,
-          decoration: const InputDecoration(
-            labelText: 'Amount',
-          ),
-        ),
-        SizedBox(height: 12),
-        const Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            'Category',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-        const SizedBox(height: 8),
-        Wrap(
-          spacing: 8,
-          runSpacing: 8,
-          children: [
-            'Food',
-            'Travel',
-            'Bills',
-            'Entertainment',
-            'Shopping',
-          ].map((category) {
-            return ChoiceChip(
-              label: Text(
-                category,
-                style: TextStyle(
-                  color: selectedCategory == category ? Colors.white : null,
-                  fontWeight: FontWeight.w600,
+        child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Expense Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Expense name is required';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              selected: selectedCategory == category,
-              selectedColor: Theme.of(context).colorScheme.primary,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 12,
-                vertical: 8,
-              ),
-              onSelected: (selected) {
-                setState(() {
-                  selectedCategory = category;
-                });
-              },
-            );
-          }).toList(),
-        ),
-        SizedBox(height: 12),
-        TextField(
-          controller: dateController,
-          readOnly: true,
-          onTap: () async {
-            final DateTime? pickedDate = await showDatePicker(
-              context: context,
-              initialDate: selectedDate,
-              firstDate: DateTime(2020),
-              lastDate: DateTime(2100),
-            );
-            if (pickedDate != null) {
-              setState(() {
-                selectedDate = pickedDate;
-                dateController.text =
-                    DateFormat('dd MMM yyyy').format(pickedDate);
-              });
-            }
-          },
-          decoration: const InputDecoration(
-            labelText: 'Date',
-          ),
-        ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            final expense = Expense(
-              name: nameController.text,
-              amount: amountController.text,
-              category: selectedCategory,
-              date: dateController.text,
-            );
+                SizedBox(height: 12),
+                TextFormField(
+                  controller: amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Amount',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Amount is required';
+                    }
 
-            if (widget.expense == null) {
-              ref.read(expenseProvider.notifier).addExpense(expense);
-            } else {
-              ref.read(expenseProvider.notifier).editExpense(
-                    widget.expense!,
-                    expense,
-                  );
-            }
+                    final amount = double.tryParse(value);
 
-            Navigator.pop(context);
-          },
-          child: Text('Save Expense'),
-        ),
-      ],
-    ));
+                    if (amount == null) {
+                      return 'Please enter a valid number';
+                    }
+
+                    if (amount <= 0) {
+                      return 'Amount must be greater than 0';
+                    }
+
+                    return null;
+                  },
+                ),
+                SizedBox(height: 12),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Category',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  children: [
+                    'Food',
+                    'Travel',
+                    'Bills',
+                    'Entertainment',
+                    'Shopping',
+                  ].map((category) {
+                    return ChoiceChip(
+                      label: Text(
+                        category,
+                        style: TextStyle(
+                          color: selectedCategory == category
+                              ? Colors.white
+                              : null,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      selected: selectedCategory == category,
+                      selectedColor: Theme.of(context).colorScheme.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 8,
+                      ),
+                      onSelected: (selected) {
+                        setState(() {
+                          selectedCategory = category;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 12),
+                TextField(
+                  controller: dateController,
+                  readOnly: true,
+                  onTap: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: selectedDate,
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime(2100),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        selectedDate = pickedDate;
+                        dateController.text =
+                            DateFormat('dd MMM yyyy').format(pickedDate);
+                      });
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Date',
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (!_formKey.currentState!.validate()) {
+                      return;
+                    }
+                    final expense = Expense(
+                      name: nameController.text,
+                      amount: amountController.text,
+                      category: selectedCategory,
+                      date: dateController.text,
+                    );
+
+                    if (widget.expense == null) {
+                      ref.read(expenseProvider.notifier).addExpense(expense);
+                    } else {
+                      ref.read(expenseProvider.notifier).editExpense(
+                            widget.expense!,
+                            expense,
+                          );
+                    }
+
+                    Navigator.pop(context);
+                  },
+                  child: Text('Save Expense'),
+                ),
+              ],
+            )));
   }
 }
